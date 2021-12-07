b90e9d1b0fe01093bc7abad9a3d96669eb9da955
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2021-12-06 18:19:48 -0800

    Metodo busqueda compatible con trabajadores

--- ABB/ArbolBinario.java
+++ ABB/ArbolBinario.java
@@ -1,34 +1,44 @@
 package limonproject.ABB;
+
 import java.nio.charset.StandardCharsets;
 
 public class ArbolBinario {
     protected Nodo raiz;
+
     public ArbolBinario() {
         raiz = null;
     }
+
     /**
      * Método que regresa el nodo raiz
+     * 
      * @param raiz
      */
     public ArbolBinario(Nodo raiz) {
         this.raiz = raiz;
     }
+
     /**
      * Regresa la raiz del arbol
+     * 
      * @return
      */
     public Nodo raizArbol() {
         return raiz;
     }
+
     /**
      * Método que verifica que el arbol no esté vacio
+     * 
      * @return boolean variable
      */
     boolean esVacio() {
         return raiz == null;
     }
+
     /**
      * Método que crea un nuevo subabrol para la estructura
+     * 
      * @param ramaIzdo
      * @param valor
      * @param ramaDcho
@@ -36,24 +46,26 @@ public class ArbolBinario {
      */
     public static Nodo nuevoArbol(Nodo ramaIzdo, Trabajador valor, Nodo ramaDcho) {
         return new Nodo(ramaIzdo, valor, ramaDcho);
-    }    
+    }
+
     /**
      * Método que recorre el árbol binario de busqueda de manera in-Orden
      * revisa el nodo izquierdo y después el derecho
      * IRD
      * http://aniei.org.mx/paginas/uam/CursoPoo/curso_poo_12.html
+     * 
      * @param raiz
      */
-    public void inOrden(Nodo r){
-        if (r!= null){
+    public void inOrden(Nodo r) {
+        if (r != null) {
             inOrden(r.izq);
-            System.out.println("Dato: "+ r.dato);
+            System.out.println("Dato: " + r.dato);
             inOrden(r.der);
         }
     }
 
-    public void inOrdenArbol(Nodo r){
-        if (r!= null){
+    public void inOrdenArbol(Nodo r) {
+        if (r != null) {
             inOrden(r.izq);
             this.agregarNodo(r, this);
             // System.out.println("Dato: "+ r.dato);
@@ -66,79 +78,82 @@ public class ArbolBinario {
      * revisa el nodo raiz, el izquierdo y luego el derecho
      * RID
      * http://aniei.org.mx/paginas/uam/CursoPoo/curso_poo_12.html
+     * 
      * @param raiz
      */
-    public void preOrden (Nodo r){
-        if((Nodo)r!= null){
-            System.out.println("Dato: "+r.dato);
+    public void preOrden(Nodo r) {
+        if ((Nodo) r != null) {
+            System.out.println("Dato: " + r.dato);
             preOrden(r.izq);
             preOrden(r.der);
         }
     }
-/**
- * Método que recorre el árbol binario de busqueda de manera post-Orden 
- * revis el nodo izquierdo, el derecho y luego la raiz
- * IDR
- * http://aniei.org.mx/paginas/uam/CursoPoo/curso_poo_12.html
- * @param raiz
- */
-    public void postOrden(Nodo r){
-        if(r!= null){
+
+    /**
+     * Método que recorre el árbol binario de busqueda de manera post-Orden
+     * revis el nodo izquierdo, el derecho y luego la raiz
+     * IDR
+     * http://aniei.org.mx/paginas/uam/CursoPoo/curso_poo_12.html
+     * 
+     * @param raiz
+     */
+    public void postOrden(Nodo r) {
+        if (r != null) {
             postOrden(r.izq);
             postOrden(r.der);
-            System.out.println("Dato: "+r.dato);
+            System.out.println("Dato: " + r.dato);
         }
     }
 
-/**
- * Método que retorna el nodo que se este buscando 
- * @param datoBusqueda
- * @return
- */
-    public Nodo busqueda(int datoBusqueda){ 
-        //raiz
+    /**
+     * Método que retorna el nodo que se este buscando
+     * 
+     * @param datoBusqueda
+     * @return
+     */
+    public Nodo busqueda(int datoBusqueda) {
+        // raiz
         Nodo aux = this.raiz;
         //
-        while(Integer.parseInt((aux.dato).toString())!= datoBusqueda)
-        {
-            if(datoBusqueda<Integer.parseInt((aux.dato).toString())){
+        while (Integer.parseInt(aux.valorNodo().getId()) != datoBusqueda) {
+            if (datoBusqueda < aux.valorNodo().getId()) {
                 aux = aux.izq;
-            }else{
+            } else {
                 aux = aux.der;
             }
-            if(aux ==null){
+            if (aux == null) {
                 System.out.println("Dato no encontrado");
-                   return null;
+                return null;
             }
         }
         System.out.println("Dato encontrado");
         return aux;
     }
 
-
-/**
- * Método que agrega nodos a un árbol de tipo Trabajador
- * @param elemento
- * @param busqueda
- */
-    public void agregarNodo(Trabajador elemento, ArbolBinario busqueda){
+    /**
+     * Método que agrega nodos a un árbol de tipo Trabajador
+     * 
+     * @param elemento
+     * @param busqueda
+     */
+    public void agregarNodo(Trabajador elemento, ArbolBinario busqueda) {
         Nodo nuevo = new Nodo(elemento);
-        if(raiz == null){
+        if (raiz == null) {
             raiz = nuevo;
-        }else{
+        } else {
             Nodo aux = raiz;
             Nodo papa;
-            while(true){
+            while (true) {
                 papa = aux;
-                if(Integer.parseInt((elemento).toString())<Integer.parseInt((aux.dato).toString())){
+                if (Integer.parseInt((elemento).toString()) < Integer.parseInt((aux.dato).toString())) {
                     aux = aux.izq;
-                    if(aux == null){
+                    if (aux == null) {
                         papa.izq = nuevo;
                         return;
                     }
-                }else{
+                } else {
                     aux = aux.der;
-                    if(aux == null){
+                    if (aux == null) {
                         papa.der = nuevo;
                         return;
                     }
