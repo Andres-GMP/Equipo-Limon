dc5661c3625d3f065980ff1b7e88afff650455e3
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2021-12-06 18:35:19 -0800

    Cambio de inserccion

--- ABB/Nodo.java
+++ ABB/Nodo.java
@@ -1,13 +1,16 @@
 package limonproject.ABB;
+
 //ando queriendo probar algo
 public class Nodo {
-    //CAMBIAR EL TIPO DE DATO
+    // CAMBIAR EL TIPO DE DATO
     protected Trabajador dato;
     protected Nodo izq;
     protected Nodo der;
-    
+
     /**
-     * Constructor que inicializa el nodo con el valor que le ingreses, las ramas izquierda y derecha son nulas
+     * Constructor que inicializa el nodo con el valor que le ingreses, las ramas
+     * izquierda y derecha son nulas
+     * 
      * @param valor
      */
     public Nodo(Trabajador valor) {
@@ -15,9 +18,10 @@ public class Nodo {
         this.izq = null;
         this.der = null;
     }
-    
+
     /**
      * Método constructor que inicializa todas las variables
+     * 
      * @param ramaIzdo
      * @param valor
      * @param ramaDcho
@@ -27,42 +31,67 @@ public class Nodo {
         izq = ramaIzdo;
         der = ramaDcho;
     }
+
+    public void insertar(Trabajador newData) {
+        if (newData.getId() < this.dato.getId()) {
+            if (izq == null)
+                izq = new Nodo(newData);
+            else
+                izq.insertar(newData);
+        } else if (newData.getId() > this.dato.getId()) {
+            if (der == null)
+                der = new Nodo(newData);
+            else
+                der.insertar(newData);
+        }
+    }
+
     /**
      * Regresa el valor del nodo
+     * 
      * @return data del nodo
      */
     public Trabajador valorNodo() {
         return dato;
     }
+
     /**
      * Regresa el nodo izquierdo
+     * 
      * @return nodo izquierdo
      */
     public Nodo subarbolIzdo() {
         return izq;
     }
+
     /**
      * Regresa el nodo derecho
+     * 
      * @return nodo derecho
      */
     public Nodo subarbolDecho() {
         return der;
     }
+
     /**
      * Asigna un nuevo valor al nodo
      */
     public void nuevoValor(Trabajador valor) {
         this.dato = valor;
     }
+
     /**
      * Asigna un nuevo valor al nodo izquierdo
+     * 
      * @param izq
      */
     public void ramaIzdo(Nodo izq) {
         this.izq = izq;
     }
+
     /**
      * Asigna un nuevo valor al nodo derecho
+     * 
      * @param der
      */
     public void ramaDcho(Nodo der) {
