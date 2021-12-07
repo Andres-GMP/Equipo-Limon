5eef5aa2243a145ac592e3be8374040e6c9b81e5
FUCHENG20 <L20760444@ensenada.tecnm.mx>
2021-12-06 17:06:10 -0800

    Cambiamos las clases
    Agregue la clase Trabajadores

---
+++ Trabajadores.java
@@ -0,0 +1,177 @@
+package ABB;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+import listagenerica.ListaGenerica;
+import sistematrabajadores.Trabajador;
+
+public class Trabajadores {
+    
+    public static void main(String[] args) throws Exception{
+        Scanner lector = new Scanner(System.in);     
+        
+        ArrayList<Integer> idList = new ArrayList<Integer>();
+        ListaGenerica listaTrabajadores = new ListaGenerica();
+
+        int opcion;
+        boolean salir = false;
+        
+
+            while(!salir){
+
+            System.out.println("\n\t+--+------MENU DE OPCIONES------------+");
+            System.out.println("\t|1.| DAR DE ALTA  (TRABAJADOR)        |");
+            System.out.println("\t|2.| DAR DE BAJA  (TRABAJADOR)        |");
+            System.out.println("\t|3.| CONSULTAS EMPLEADO ORDEN/ALTA    |");
+            System.out.println("\t|4.| CONSULTAS EMPLEADO ORDEN/POR ID  |");
+            System.out.println("\t|5.| MODIFICAR DATOS DE UN TRABAJADOR |");
+            System.out.println("\t|6.| SALIR DEL SISTEMA                |");
+            System.out.println("\t+--+----------------------------------+");
+
+            try{
+            System.out.println("\t|        SELECCIONA UNA OPCION        |");
+            System.out.println("\t+--+----------------------------------+");
+            opcion = lector.nextInt();
+                
+            switch(opcion){
+
+                //CASO #1 DAR DE ALTA AL TRABAJADOR
+                case 1:
+                
+                //AUTOGENERAR ID
+                Random aleatorio = new Random();
+                int id = 1000 + aleatorio.nextInt(8999);
+
+                //PREGUNTA SI LA ID ESTA REPETIDO EN EL ARRAY
+                while (idList.contains(id)){
+                    id = 1000 + aleatorio.nextInt(8999);
+
+                }
+                //SI EN LA LISTA NO CONTIENE EL ID SE AGREGA ESTE
+                idList.add(id);
+
+                try{
+
+                System.out.println("\n\tINTRODUCIR TU NOMBRE");
+                String nom = lector.next();
+
+                System.out.println("\tINTRODUCIR TU APELLIDO PATERNO");
+                String apeP = lector.next();
+
+                System.out.println("\tINTRODUCIR TU APELLIDO MATERNO");
+                String apeM = lector.next();
+
+                System.out.println("\tINTRODUCIR TU EDAD");
+                int edad = lector.nextInt();
+
+                System.out.println("\tINTRODUCIR TU GENERO (M/F)");
+                String gene = lector.next();
+
+                System.out.println("\tINTRODUCIR TU SUELDO");
+                double suel = lector.nextDouble();
+
+                System.out.println("\tINTRODUCIR TU PUESTO");
+                String pues = lector.next();
+                
+                //INSERTAMOS A LA LISTA UN NUEVO TRABAJADOR
+                listaTrabajadores.insertarInicio(new Trabajador(nom, apeP , apeM , edad , gene , id , suel , pues)); 
+
+                System.out.println("\tHAS INSERTADO CORRECTAMENTE");
+                    
+                }catch(InputMismatchException e ){
+                System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("\t| DEBES DE INTRODUCIR DEL TIPO DE DATO QUE SE SOLICITA |");
+                System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                lector.next();
+                }
+                
+                break;
+
+                //CASO #2 DAR DE BAJA AL TRABAJADOR
+                case 2:
+
+                System.out.println("\n\tINTRODUCE EL ID DEL TRABAJADOR A ELIMINAR (4 DIGITOS)");
+                int ide = lector.nextInt();
+
+                System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("\tESTAS SEGURO DE ELIMINAR AL TRABAJADOR ? (SI/NO)");
+                System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                String opciones = lector.next();
+
+                try{
+                if(opciones.equals("SI"))
+                {
+                    listaTrabajadores.eliminar(ide);
+                    System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("\t|   SE HA ELIMINADO CORRECTAMENTE     |");
+                    System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
+                else if (opciones.equals("NO"))
+                {
+                    System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("\t|      SE HA SALIDO DEL SISTEMA       |");
+                    System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
+                else
+                {
+                    System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    System.out.println("\t|      ESTA OPCION NO ES VALIDA       |");
+                    System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
+                    }catch(Exception e){
+                System.out.println("\n\t NO EXISTE EL ID DEL TRABAJADOR ");
+                }
+                break;
+
+                //CASO #3 MOSTRAMOS LA TABLA DE LOS TRABAJADORES EN ORDE QUE LOS INSERTE
+                case 3:
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CONSULTA DE TRABAJADORES~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                listaTrabajadores.imprimir();
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                break;
+
+                //CASO #4 MOSTRAMOS LA TABLA DE LOS TRABAJADORES EN ORDEN ASCENDENTE POR ID
+                case 4:
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CONSULTA DE TRABAJADORES POR ID~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                listaTrabajadores.Lista().burbuja().imprimir();
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                break;
+
+                //CASO #5 MODIFICACION A LOS DATOS DE LOS TRABAJADORES
+                case 5:
+                try{
+                System.out.println("\n\tINTRODUCE EL ID DEL TRABAJADOR A MODIFICAR (4 DIGITOS)");
+                ide = lector.nextInt();
+
+                listaTrabajadores.modificar(ide);
+                }catch(Exception e){
+                System.out.println("\n\t NO EXISTE EL ID DEL TRABAJADOR ");
+                }
+                break;
+
+                case 6:
+                salir = true;
+                break;
+
+                default:
+                System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("\t|      ESTA OPCION NO ES VALIDA       |");
+                System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+            }
+
+        }catch(InputMismatchException e ){
+            System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println("\t|     DEBES DE INTRODUCIR UN NUMERO   |");
+            System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+            lector.next();
+        
+        }
+
+      }
+    }
+
+}
