fa1114bfafa3107b7194e257d8c6e97813763ccd
AxelTEC <95446278+AxelTEC@users.noreply.github.com>
2021-12-08 16:32:33 -0800

    Try catch

--- Trabajadores.java
+++ Trabajadores.java
@@ -231,11 +231,11 @@ public class Trabajadores {
 
                 }
 
-            } catch (InputMismatchException e) {
+            } catch (Exception e) {
                 System.out.println("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("\t|     DEBES DE INTRODUCIR UN NUMERO   |");
                 System.out.println("\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-                lector.readLine();
+                // lector.readLine();
 
             }
 
