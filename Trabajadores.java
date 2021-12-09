ea9cb62a5a560b49c7edf1008fa64d291571ab6f
Andres-GMP <al20760260@ite.edu.mx>
2021-12-08 21:01:12 -0800

    ajuste a método trabajdoresPrueba (3)

--- Trabajadores.java
+++ Trabajadores.java
@@ -446,7 +446,7 @@ public class Trabajadores {
         arbolTrabajadores.agregarNodo(new Trabajador("Oscar", "Anguiano", "Gonzalez", 19, "M", 11, 18000, "VENTAS"));
         arbolTrabajadores.agregarNodo(new Trabajador("Andres", "Gonzalez", "Martinez", 19, "M", 5, 4000, "VENTAS"));
         arbolTrabajadores.agregarNodo(new Trabajador("Omar", "Millan", "Valenzuela", 20, "M", 3, 4000, "RHH"));
-        arbolTrabajadores.agregarNodo(new Trabajador("Pablo", "Palma", "Garcia", 20, "M", 4, 4000, "CEO"));
+        arbolTrabajadores.agregarNodo(new Trabajador("Pablo", "Palma", "Garcia", 20, "M", 6, 4000, "CEO"));
         System.out.println("Trabajadores agregados Automaticamente...");
     }
 
